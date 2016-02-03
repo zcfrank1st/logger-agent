ab6462b68d7be5dd8d0a37befeb3a0a231f6a05e
zcfrank1st <zcfrank1st@sina.com>
2016-02-03 15:20:50 +0800

    add method log

--- src/main/java/com/dejionline/agent/logger/interceptor/LogInterceptor.java
+++ src/main/java/com/dejionline/agent/logger/interceptor/LogInterceptor.java
@@ -1,11 +1,11 @@
 package com.dejionline.agent.logger.interceptor;
 
-import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.Origin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by zcfrank1st on 2/2/16.
@@ -13,8 +13,11 @@ import java.lang.reflect.Method;
 public class LogInterceptor {
     public static Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
 
-    public static void interceptor(@Origin Method method, @AllArguments String[] args ) {
-        // TODO 打印方法参数等相关信息
-        logger.info(method.getName());
+    public static void interceptor(@Origin Method method, @Origin Class clazz) {
+        logger.info(
+                        "class:" + clazz.getCanonicalName() + "\t" +
+                        "method:" + method.getName() + "\t" +
+                        "args:" + Arrays.toString(method.getParameters())
+        );
     }
 }
